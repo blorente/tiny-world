2ec9a361069c781fe9b302628c2f56ff1dce1521
Borja Lorente <blorente.me@gmail.com>
2021-02-12 12:09:32 +0000

    some fixes

--- scripts/process_file.py
+++ scripts/process_file.py
@@ -9,10 +9,10 @@ from typing import Dict
 def load_map(mapfile: Path) -> Dict[str, str]:
     return yaml.safe_load(mapfile.open())
 
-def process_links(original: str, concept_map: Dict[str, str]) -> str:
+def process_links(original: str, concept_map: Dict[str, str], jumps_up: str) -> str:
     processed = original
     for (concept, link) in concept_map.items():
-        link = link.replace(".md", ".html")
+        link = "{}/{}".format(jumps_up, link.replace(".md", ".html"))
         processed = processed.replace(f"@{concept}", f"[{concept}]({link})")
     return processed
 
@@ -21,5 +21,7 @@ mapfile=Path(sys.argv[1])
 infile=Path(sys.argv[2])
 
 concept_map = load_map(mapfile)
+rel_to_root = infile.relative_to(mapfile.parent)
+jumps_up = "/".join([".." for i in range(len(rel_to_root.parts) - 1)])
 # print(concept_map, file=sys.stderr)
-print(process_links(infile.read_text(), concept_map))
+print(process_links(infile.read_text(), concept_map, jumps_up))
